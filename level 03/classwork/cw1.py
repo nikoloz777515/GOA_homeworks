f643333227b87d690716d7df0aa862590c18ad9d
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-04-05 00:16:32 +0400

    commit

---
+++ level 03/classwork/cw1.py
@@ -0,0 +1,7 @@
+#1) შექმენით პროგრამა სადაც, მომხმარებელს შემოატანინებთ 2 რიცხვს, შემოტანის შემდეგ და შეინახეთ ისინი ცვლადებში და შეადარეთ ერთმანეთს, გამოიყენეთ if-else თუ ერთი იქნება მეტი მეორეზე დაბეჭდეთ პირველი რიცხვი მეტია მეორეზე, თუ მეორე იქნება მეტი პირიქით
+num1=int(input("please enter first number:" ))
+num2=int(input("please enter second number:"))
+if num1 > num2 :
+    print("num1>num2")
+else:
+    print("naklebia num2 ze")
