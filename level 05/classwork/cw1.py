0ee08a95d78fb2df1663e53f8080e8066e584f08
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-04-06 00:17:14 +0400

    commit

--- level 05/classwork/cw1.py
+++ level 05/classwork/cw1.py
@@ -1,13 +1,17 @@
 # shows your name
+
 name = input ("please enter first name: ")
 print("entered value is: " + name )
 #enter your last name 
+
 lastname = input ("please enter last name: ")
 print("entered value is: " + lastname)
 #shows your age 
+
 age = input ("please enter your age: ")
 print("entered value is: " + age)
 # davaleba 2
+
 num1 = int(input("please enter first number: "))
 num2 = int(input("please enter second number: "))
 
@@ -15,6 +19,7 @@ print(num1 + num2)
 print(num1 - num2)
 print(num1 * num2)
 print(num1 / num2)
+
 #davaleba 3
 
 # BONUS:
