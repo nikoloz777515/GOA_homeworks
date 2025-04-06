cf8b4443b4e47e01afb0311133a992add79868f1
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-04-06 17:58:31 +0400

    commit

---
+++ level 05/homework/hw1.py
@@ -0,0 +1,37 @@
+#2)  ცვლადში შეინახეთ პაროლი, შემდეგ კი მომხმარებელს შემოატანინეთ პაროლი. იმ შემთხვევაში, თუ მომხმარებლის მიერ შემოტანილი პაროლი სწორია, დაბეჭდეთ "access granted". წინააღმდეგ შემთხვევაში - "access denied".
+
+#3) მომხმარებელს შემოატანინეთ რიცხვი და დაწერეთ ისეთი პროგრამა, რომელიც განსაზღვრავს ეს რიცხვი ლუწია თუ კენტი.
+
+#4) მომხმარებელს შემოატანინეთ 5 რიცხვი. დაწერეთ პროგრამა, რომელიც გამოთვლის და დაბეჭდავს ამ რიცხვების საშუალო არითმეტიკულს.
+
+#5) მომხმარებელს შემოატანინეთ ტემპერატურა ცელსიუსში. დაწერეთ პროგრამა, რომელიც მომხმარებლის მიერ შემოტანილ ტემპერატურას გადაიყვანს ფარენგეიტში და დაბეჭდეთ საბოლოო შედეგი. (მოიძიეთ ფორმულა ინტერნეტში)
+
+#6) მეხუთე დავალების მსგავსად, შემოატანინეთ მომხმარებელს ტემპერატურა, თუმცა ამჯერად ფარენგეიტში. შემდეგ კი დაწერეთ პროგრამა, რომელიც შემოტანილ ტემპერატურას ცელსიუსში გადაიყვანს და საბოლოოდ დაბეჭდეთ შედეგი.
+
+# davaleba 2
+password=input("please enter your password: ")
+if password <= "Nikolozi123":
+  print(   password , "access granted")
+else:("pow")
+print( pow, "access denied")
+
+
+
+
+
+num1 = int(input("please enter first number odd  : "))
+num2 = int(input("please enter second number even  : "))
+print("entered value is: first number  ")
+print("entered value is: second number ")
+
+         
+ 
+
+
+num1 = int (input("please enter first number" ))
+num2 = int (input('please enter second number'))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
