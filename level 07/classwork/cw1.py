d6f3e84af7479027722cdae1313503f1379a2278
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-04-07 22:42:57 +0400

    commit

---
+++ level 07/classwork/cw1.py
@@ -0,0 +1,14 @@
+#1) შექმენით პროგრამა, რომელიც მომხმარებელს შემოატანინებს 2 რიცხვს, ამ ორ რიცხვს შორის გამოიყენეთ ყველა არითმეტიკული და შედარების ოპერატორები
+num1=int(input("please enter first number: "))
+num2=int(input("please enter second number: "))
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+
+num1=int(input("please enter first number: "))
+num2=int(input("please enter second number: "))
+
+print(num1<num2)
+print(num1>=num2)
