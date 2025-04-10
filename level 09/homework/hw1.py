35cffaaae8d7f2a905b338322c6408e936998a58
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-04-10 14:07:10 +0400

    commit

---
+++ level 09/homework/hw1.py
@@ -0,0 +1,31 @@
+#1) მომხმარებელს შემოატანინეთ სახელი და გვარი, რასაც შემდეგ გადაუვლით for loop-ით და შემოტანილი სახელისა და გვარის თითოეულ ასოს ცალ-ცალკე გამოიტანთ ტერმინალში;
+
+#) მომხმარებელს შემოატანინეთ რიცხვი და დაბეჭდეთ ამ რიცხვის თითოეული ციფრი;
+
+#3) მომხმარებელს შემოატანინეთ რიცხვი და დაბეჭდეთ რიცხვები მომხმარებლის შემოტანილი რიცხვიდან 0-მდე. (მაგ. input = 3. output: 3 2 1 );
+
+#4) კომენტარის სახით ახსენით თუ როგორ მუშაობს for loop-ი;
+
+#5) კომენტარის სახით განმარტეთ თუ რას ნიშნავს საიტერაციო ცვლადი.
+
+#davaleba1
+#num=input("please enter your full name: ")
+#for i in range (num):
+ #  print(i)
+
+#davaleba2
+num=int(input("please enter number: "))
+for i in range (num):
+    print(i)
+
+#davaleba3
+num+int(input("please enter number: "))
+for i in range (num):
+  num=num-i
+  print(num)
+
+#davaleba4 # 
+# loop aris cikli  is rac maaglitad ragacis 100 jer daweras gvtxoven da argvinda rom es ragac 100 jer davwerot davixmarebt loop s da mivutitebt iqve ramdenjer gvinda ro dabejdos da imdenjer dabechdavs ramdenjerac vetyvit da es advili iqneba developeristvis radgan is dazogavs dros 
+
+#davaleba5 
+# sainteracio cvladi aris cikli anu kodis meored gamotana an tanmimdevroba
