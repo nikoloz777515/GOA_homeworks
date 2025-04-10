ebaf56a7e83ac50228c7a17cb671ea7e23737e15
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-04-10 14:57:53 +0400

    Update hw1.py

--- level 09/homework/hw1.py
+++ level 09/homework/hw1.py
@@ -2,16 +2,16 @@
 
 #) მომხმარებელს შემოატანინეთ რიცხვი და დაბეჭდეთ ამ რიცხვის თითოეული ციფრი;
 
-#3) მომხმარებელს შემოატანინეთ რიცხვი და დაბეჭდეთ რიცხვები მომხმარებლის შემოტანილი რიცხვიდან 0-მდე. (მაგ. input = 3. output: 3 2 1 );
+#3) გ. input = 3. oმომხმარებელს შემოატანინეთ რიცხვი და დაბეჭდეთ მომხმარებლის შემოტანილი რიცხვიდან 0-მდე. (მაutput: 3 2 1 );რიცხვები 
 
 #4) კომენტარის სახით ახსენით თუ როგორ მუშაობს for loop-ი;
 
 #5) კომენტარის სახით განმარტეთ თუ რას ნიშნავს საიტერაციო ცვლადი.
 
 #davaleba1
-#num=input("please enter your full name: ")
-#for i in range (num):
- #  print(i)
+num=input("please enter your full name: ")
+for  loop in (num):
+  print(num)
 
 #davaleba2
 num=int(input("please enter number: "))
@@ -19,10 +19,9 @@ for i in range (num):
     print(i)
 
 #davaleba3
-num+int(input("please enter number: "))
-for i in range (num):
-  num=num-i
-  print(num)
+num=int(input("please enter number: "))
+for i in range (num, 0,-1):
+  print(i)
 
 #davaleba4 # 
 # loop aris cikli  is rac maaglitad ragacis 100 jer daweras gvtxoven da argvinda rom es ragac 100 jer davwerot davixmarebt loop s da mivutitebt iqve ramdenjer gvinda ro dabejdos da imdenjer dabechdavs ramdenjerac vetyvit da es advili iqneba developeristvis radgan is dazogavs dros 
