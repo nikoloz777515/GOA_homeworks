385d67faea229f9336e1aea5ea5e7c7cec7c0b51
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-04-11 22:22:19 +0400

    commit

--- level 10/classwork/cw1.py
+++ level 10/classwork/cw1.py
@@ -1,7 +1,7 @@
 #დაწერეთ პროგრამა, რომელიც მომხმარებლს მოსთხოვს, რომ შემოიტანოს დადებითი რიცხვი. 
 #თუ მომხმარებელი შემოიტანს უარყოფით რიცხვს ან ნულს, პროგრამამ უნდა მოსთხოვოს რიცხვის თავიდან შემოტანა
-num=int(input("please enter possitive numbers: "))
+num=int(input("please enter  number: "))
 while num <=0:
-    print("wrong possitive number try again")
+    print("wrong possitive number. try again")
     num=int(input("please enter possitive numbers: "))
-    print("your number was possitive")
+print("your number was possitive.")
