b8f91983ff7dabd96b7d817eec3f57534b0f4103
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-04-11 22:15:58 +0400

    commit

--- level 10/classwork/cw1.py
+++ level 10/classwork/cw1.py
@@ -1,7 +1,7 @@
 #დაწერეთ პროგრამა, რომელიც მომხმარებლს მოსთხოვს, რომ შემოიტანოს დადებითი რიცხვი. 
 #თუ მომხმარებელი შემოიტანს უარყოფით რიცხვს ან ნულს, პროგრამამ უნდა მოსთხოვოს რიცხვის თავიდან შემოტანა
-user_num=int(input("please enter possitive number: "))
-while user_num <=0:
-    print("ypur entered number isn't valid. try again")
-    num=int(input("please enter possitive number: "))
-print("your number was possitive.")
+num=int(input("please enter possitive numbers: "))
+while num <=0:
+    print("wrong possitive number try again")
+    num=int(input("please enter possitive numbers: "))
+    print("your number was possitive")
---
+++ level 10/homework/hw1.py
@@ -0,0 +1,13 @@
+#1) აუცილებლად გადახედეთ ჩანაწერს;
+
+#2) მოიყვანეთ while loop-ის მაგალითი და კომენტარის სახით ახსენით, თუ როგორ მუშაობს while ციკლი;
+
+#3) კომენტარის სახით ახსენით თუ რა განსხვავებაა for loop-სა და while loop-ს შორის;
+
+#4) ცვლადში შეინახეთ პაროლი. შემდეგ მომხმარებელს შემოატანინეთ პაროლი, სანამ მომხმარებლის მიერ შემოტანილი პაროლი არ დაემთხვევა თქვენს პაროლს, გამოუტანეთ "Incorrect password. Try again". იმ შემთხვევაში თუ ეს პაროლები ერთმანეთს დაემთხვევა გამოიტანეთ "Correct password. You have successfully logged in." (გააკეთეთ While ციკლით, არ გამოიყენოთ if else statement-ები.);
+
+#5) ტერმინალში დაბეჭდეთ რიცხვები 1-დან 20-ის ჩათვლით ორ-ორის გამოტოვებით.
+
+#6) Sololearn-ში გაიარეთ While loops სექცია:
+#1 done
+#2 while loop mushaobs manam sanam mnishvneloba ar daibechdeba da ar morcheba manamde imushavebs while loop magalitad 
