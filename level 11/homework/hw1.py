8cfd91a3ef0839a15ef71260db0fa138eeaedbdd
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-04-13 18:58:16 +0400

    Update hw1.py

--- level 11/homework/hw1.py
+++ level 11/homework/hw1.py
@@ -1,11 +1,21 @@
 # 1) შექმენით Number guess game, if else statement-ების გამოყენებით.
-guess_number = 6 
-user_input = 0
-if input != guess_number:
- user_input = int(input("please enter a number 1-10: "))
-# 2) შექმენით Number guess game, while ციკლების გამოყენებით.
+# guess_number=5
+# user_input=int(input("please enter a number 1,10: "))
+# if user_input != guess_number:
+#  print("wrong number try again")
+# else:
+#  user_input = guess_number
+#  print("you guess number you won")
 
-# 3) გაიხსენეთ გაკვეთილზე გაკეთებული დავალება მომხმარებლის სისტემაში შესვლის მცდელობაზე და დამოუკიდებლად გააკეთეთ იგივე while ციკლის საშუალებით.
+# 2) შექმენით Number guess game, while ციკლების გამოყენებით.
+#guess game 
+guess_number=4
+user_input=0
+
+while user_input != guess_number:
+    user_input=int(input("please enter a number (1,10): "))
+print("you guess number")
+# 3) გაიხსენეთ გაკვეთილზე გაკეთებული დავალება მომხმარებლის სისტემაში შესვლის მცდელობაზე და დამოუკიდებლად გააკეთეთ იგივე while ციკლის საშუალებით
 
 # 4) გადაუარეთ სტრინგს for loop-ით და სტრი გში მხოლოდ ლუწ ინდექსებზე მდგომი ასოები დაბეჭდეთ .
 
