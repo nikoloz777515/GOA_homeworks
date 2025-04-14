04fd1f1bf6db81e00d91a2c5d9c71bdc01050fee
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-04-14 15:32:48 +0400

    Update hw1.py

--- level 11/homework/hw1.py
+++ level 11/homework/hw1.py
@@ -1,22 +1,27 @@
 # 1) შექმენით Number guess game, if else statement-ების გამოყენებით.
-# guess_number=5
-# user_input=int(input("please enter a number 1,10: "))
-# if user_input != guess_number:
-#  print("wrong number try again")
-# else:
-#  user_input = guess_number
-#  print("you guess number you won")
+# guess_number=7
 
-# 2) შექმენით Number guess game, while ციკლების გამოყენებით.
-#guess game 
-# guess_number=4
 # user_input=0
 
-# while user_input != guess_number:
-#     user_input=int(input("please enter a number (1,10): "))
-# print("you guess number")
-# 3) გაიხსენეთ გაკვეთილზე გაკეთებული დავალება მომხმარებლის სისტემაში შესვლის მცდელობაზე და დამოუკიდებლად გააკეთეთ იგივე while ციკლისბ საშუალეით
-
+# if user_input != guess_number: 
+# else:
+#    user_input=int(input("enter a number: 1,10 "))
+# 2) შექმენით Number guess game, while ციკლების გამოყენებით. done
+# guess_number=4
+# user_input=0
+# while user_input !=guess_number:
+#     user_input=int(input("enter a number: 1,10 "))
+# 3) გაიხსენეთ გაკვეთილზე გაკეთებული დავალე
+# ბა მომხმარებლის სისტემაში შესვლის მცდელობაზე და დამოუკიდებლად გააკეთეთ იგივე while ციკლისბ საშუალეით #done
+# email =" ari@gmail.com"
+# password = "ari777"
+# user_email = ""
+# user_password=""
+# while user_email != email and user_password != password:
+#     user_password = input("please enter password: ")
+#     user_email = input("please enter email: ")
 # 4) გადაუარეთ სტრინგს for loop-ით და სტრი გში მხოლოდ ლუწ ინდექსებზე მდგომი ასოები დაბეჭდეთ .
-
+name="nikolozi"
+for i in range(name):
+    print(i)
 # 5) გამოიყენეთ range და ლუწ ინდექსებზე მდგომი რიცხვები შეკრიბეთ. ჯამი გამოიტანეთ ტერმინალში.
