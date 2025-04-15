f2c803b957d33de272283d735a6076a502df02e7
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-04-15 12:26:38 +0400

    commit

--- level 11/homework/hw1.py
+++ level 11/homework/hw1.py
@@ -1,27 +1,101 @@
 # 1) შექმენით Number guess game, if else statement-ების გამოყენებით.
 # guess_number=7
 
-# user_input=0
+# user_input=int(input("enter a number: 1,10 "))
+# while guess_number != user_input:
+#     user_input=int(input("enter again: "))
+# if guess_number == user_input:
+#     print("correct")
 
-# if user_input != guess_number: 
-# else:
-#    user_input=int(input("enter a number: 1,10 "))
+
 # 2) შექმენით Number guess game, while ციკლების გამოყენებით. done
 # guess_number=4
-# user_input=0
+# user_input=int(input("please enter number: ")
 # while user_input !=guess_number:
 #     user_input=int(input("enter a number: 1,10 "))
+
+
 # 3) გაიხსენეთ გაკვეთილზე გაკეთებული დავალე
 # ბა მომხმარებლის სისტემაში შესვლის მცდელობაზე და დამოუკიდებლად გააკეთეთ იგივე while ციკლისბ საშუალეით #done
 # email =" ari@gmail.com"
 # password = "ari777"
-# user_email = ""
-# user_password=""
+# user_email =input("please enter email")
+# user_password=input("please enter password")
 # while user_email != email and user_password != password:
 #     user_password = input("please enter password: ")
 #     user_email = input("please enter email: ")
+
+
 # 4) გადაუარეთ სტრინგს for loop-ით და სტრი გში მხოლოდ ლუწ ინდექსებზე მდგომი ასოები დაბეჭდეთ .
-name="nikolozi"
-for i in range (len(name)):
-    print(i)
+# name="nikolozi"
+# for i in range (len(name)) :
+#     if i % 2==0 :
+#         print(name[i])
 # 5) გამოიყენეთ range და ლუწ ინდექსებზე მდგომი რიცხვები შეკრიბეთ. ჯამი გამოიტანეთ ტერმინალში.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# num=[10,20,5,30,25,40]
+# sum=0
+# for i in range(len(num)):
+#     if  i % 2 == 0:
+#         sum += num[i]
+# print(sum)
