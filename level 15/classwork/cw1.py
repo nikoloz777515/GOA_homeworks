63817f953e5c04e6ecddeb588327c73922d502b1
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-04-20 21:51:22 +0400

    commit

---
+++ level 15/classwork/cw1.py
@@ -0,0 +1,1 @@
+#  orive gaketebulia
---
+++ level 15/homework/hw1.py
@@ -0,0 +1,34 @@
+# 2) გადახედეთ ჩანაწერს აუცილებლად, რადგან უკეთ გაიაზროთ ფუნქციები პითონში. done
+
+# 3) დარეგისტრირდით https://www.codewars.com-ზე. done
+
+# 4) მომხმარებელს შემოატანინეთ წინადადება. ტერმინალში კი დაბეჭდეთ ეს წინადადება ისე რომ იყოს:
+# user_input=(input("please enter your name: "))
+# print(user_input.lower())
+
+# print(user_input.upper())
+
+# print(user_input.capitalize()) done
+
+
+# 1. ყველა პატარა ასო;
+# 2. ყველა დიდი ასო;
+# 3. პირველი ასო დიდი, ხოლო ყველა დანარჩენი პატარა.
+
+
+# 5) გააკეთეთ მეოთხე დავალება, თუმცა ამჯერად ჩააშენეთ კოდი ფუნქციაში.
+# myname= "Niko"
+# user_input=input("please enter your name: ")
+# if user_input != myname:
+#     print("try again")
+# else:
+#     print("your name is right")
+
+
+# გააკეთეთ შემდეგი Codewars-ები:
+
+# 6) https://www.codewars.com/kata/555a67db74814aa4ee0001b5/train/python
+
+# 7) https://www.codewars.com/kata/56200d610758762fb0000002/train/python
+
+# 8) https://www.codewars.com/kata/55685cd7ad70877c23000102/train/python
