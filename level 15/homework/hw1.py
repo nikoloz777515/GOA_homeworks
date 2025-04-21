1f148c6765596bf824185f3ad7fd62187adf8f03
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-04-21 19:13:49 +0400

    commit

--- level 15/homework/hw1.py
+++ level 15/homework/hw1.py
@@ -19,16 +19,29 @@
 # 5) გააკეთეთ მეოთხე დავალება, თუმცა ამჯერად ჩააშენეთ კოდი ფუნქციაში.
 # myname= "Niko"
 # user_input=input("please enter your name: ")
-# if user_input != myname:
+# if user_input.capitalize() != myname:
 #     print("try again")
 # else:
-#     print("your name is right")
+#     print("your name is right")  done
 
 
 # გააკეთეთ შემდეგი Codewars-ები:
 
 # 6) https://www.codewars.com/kata/555a67db74814aa4ee0001b5/train/python
+# def is_even(n): 
+#     if n % 2 ==0:
+#         return True
+#     else:
+#         return False
+
+
+
 
 # 7) https://www.codewars.com/kata/56200d610758762fb0000002/train/python
+# def add_five(num):
+#     total = + 5
+#     return total
+
+
 
 # 8) https://www.codewars.com/kata/55685cd7ad70877c23000102/train/python
---
+++ level 16/classwork/cw1.py
@@ -0,0 +1,1 @@
+# vergavakete vercerti
