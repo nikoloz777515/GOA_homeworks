0cb92813137160a249afbec748f6f8bdc7c5784c
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-04-22 23:46:26 +0400

    Update hw1.py

---
+++ level 16/homework/hw1.py
@@ -0,0 +1,13 @@
+გააკეთეთ Codewars-ები:
+
+# 1) https://www.codewars.com/kata/565f5825379664a26b00007c/train/python
+
+# 2) https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/python
+
+# 3) https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/python
+
+# 4) https://www.codewars.com/kata/52ceafd1f235ce81aa00073a/train/python
+
+# 5) https://www.codewars.com/kata/511f0fe64ae8683297000001/train/python
+
+# 6) https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/train/python
