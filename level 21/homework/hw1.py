e15b59d3d6d7e288bcad6c3545a144a20b48f7ce
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-05-01 19:27:11 +0400

    commit

---
+++ level 21/homework/hw1.py
@@ -0,0 +1,13 @@
+# გააკეთეთ Codewars-ები:
+
+# 8 kyu:
+# 1) https://www.codewars.com/kata/57faece99610ced690000165
+# 2) https://www.codewars.com/kata/57fb09ef2b5314a8a90001ed
+# 3) https://www.codewars.com/kata/59dd3ccdded72fc78b000b25
+# 4) https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89
+# 5) https://www.codewars.com/kata/58bf9bd943fadb2a980000a7
+
+
+# 7kyu:
+# 1) https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/python
+# 2) https://www.codewars.com/kata/58712dfa5c538b6fc7000569/train/python
