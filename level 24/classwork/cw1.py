3d6a8abe6d6279351a4bbfc984981636c567fc9a
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-05-05 14:32:09 +0400

    commit

--- level 24/classwork/cw1.py
+++ level 24/classwork/cw1.py
@@ -1,73 +1,69 @@
 # 7 kyu
 # https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/python
 
-
 # def accum(st):
-#     st = st.lower()
-#     result = []
-    
-#     for index in range(len(st)):
-#         word=st[index] * (index + 1)
-#         result.append(word.capitalize())
-#     return "-".join(result)
+#     str = ""
+#     count = 0
+#     for i in st:
+#         str+=i.upper()+i.lower() * count + "-"
+#         count+=1
+        
+#     return str[0:-1]
+# 7 kyu
 # https://www.codewars.com/kata/559590633066759614000063/train/python
-
-
 # def min_max(lst):
-#     max_number= lst [0]
-    
-    
-#     for number in lst:
-#         if max_number < number:
-#             max_number = number
+#     min=lst[0]
+#     max=lst[0]
+#     result=[]
+#     for i in lst:
+#         if max < i:
+#             max=i
             
-#     min_number=lst[0]
-    
-    
-#     for number in lst:
-#         if min_number > number:
-#             min_number = number
-
+#         elif min> i:
+#             min=i
+#     result.append(min)
+#     result.append(max)
+#     return result
 
 # https://www.codewars.com/kata/583f158ea20cfcbeb400000a/train/python
-
-
 # def arithmetic(a, b, operator):
-    
-    
-#     match operator:
-#         case "add":
-#             return a + b
-#         case "subtract":
-#             return a - b
-#         case "multiply":
-#             return a * b
-#         case "divide":
-#             return a / b
-
-
+#     if operator == "add":
+#         return a+b 
+#     elif operator=="subtract":
+#         return a-b
+#     elif operator=="multiply":
+#         return a*b
+#     else:
+#         return a/b
 # https://www.codewars.com/kata/566fc12495810954b1000030/train/python
 
-
 # def nb_dig(n, d):
-    
-#     numbers = []
-#     count = 0
-    
-    
-#     for number in range(n + 1):
-#         numbers.append(str(number ** 2 ))
-        
-        
-#     d = str(d)
-        
-#     for number in numbers:
-#         count += number
-
+#     num = []
+#     for i in range(0,n+1):
+#         num.append(i**2)
+#     num = str(num)
+#     return num.count(str(d))
 
 # https://www.codewars.com/kata/511f11d355fe575d2c000001/train/python
-
-
 # def two_oldest_ages(ages):
-#     ages.sort()
-#     return[ages[-2], ages[-1]]
+
+#     max1 = 0
+
+#     max2 = 0 
+
+
+
+#     for i in ages:
+
+#         if i > max1:
+
+#             max2 =max1
+
+#             max1 = i
+
+#         elif i > max2:
+
+#             max2 = i
+
+#     return [max2,max1]
+ 
---
+++ level 24/homework/hw1.py
@@ -0,0 +1,11 @@
+# 8 kyu
+# https://www.codewars.com/kata/5ad0d8356165e63c140014d4/train/python
+# https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/python
+
+# 7 kyu
+# https://www.codewars.com/kata/57a049e253ba33ac5e000212/train/python
+# https://www.codewars.com/kata/5300901726d12b80e8000498/train/python
+# https://www.codewars.com/kata/5d5ee4c35162d9001af7d699/train/python
+
+# რა არის რიცხვის ფაქტორიალი
+# https://en.wikipedia.org/wiki/Factorial
