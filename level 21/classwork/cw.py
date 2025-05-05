4df61e8a2a8d5fb6719d6a8af603535c4930f1b1
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-05-05 19:34:41 +0400

    commit

---
+++ level 21/classwork/cw.py
@@ -0,0 +1,9 @@
+# 1) https://www.codewars.com/kata/5467e4d82edf8bbf40000155/train/python
+
+# მინიშნება: How to sort numbers list in desceding order in py?
+
+# how to transform number in string? or in reverse in py 
+
+# 2) https://www.codewars.com/kata/55908aad6620c066bc00002a/train/python
+
+# 3) https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/train/python
