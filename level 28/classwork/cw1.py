76d774db3fb5deaf98f3d31e469a58f2942e9a0a
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-05-09 14:02:59 +0400

    commit

---
+++ level 28/classwork/cw1.py
@@ -0,0 +1,42 @@
+# 7 kyu
+# https://www.codewars.com/kata/5356ad2cbb858025d800111d/train/python
+# def cap_me(names):
+#     result=[]
+    
+    
+#     for name in names:
+#         result.append(name.lower().capitalize())
+#     return result
+    
+
+# https://www.codewars.com/kata/54b81566cd7f51408300022d/train/python
+# def word_search(query, words):
+#     result = []
+#     query = query.lower()
+    
+    
+#     for word in words:
+#         if query in word.lower():
+#             result.append(word)
+            
+#     if len(result) == 0:
+#         return ["None"]
+    
+#     return result
+
+# 6 kyu
+# https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/train/python
+# def duplicate_count(text):
+#     count = 0
+#     seen = []
+#     text = text.lower()
+    
+#     for char in text:
+#         if text.count(char) > 1 and char not in seen:
+#             count+=1
+#             seen.append(char)
+#     return count
+# https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/python
+
+
+# https://www.codewars.com/kata/517abf86da9663f1d2000003/train/python
---
+++ level 28/homework/hw1.py
@@ -0,0 +1,12 @@
+# 8 kyu
+# https://www.codewars.com/kata/5966e33c4e686b508700002d/train/python
+# https://www.codewars.com/kata/572b6b2772a38bc1e700007a/train/python (ord - ფუნქციაზე გადახედეთ შემდეგ რესურს https://www.w3schools.com/python/ref_func_ord.asp)
+
+# 7 kyu
+# https://www.codewars.com/kata/52f3149496de55aded000410/train/python
+# https://www.codewars.com/kata/56a921fa8c5167d8e7000053/train/python
+# https://www.codewars.com/kata/56582133c932d8239900002e/train/python
+
+# 6 kyu
+# https://www.codewars.com/kata/5264d2b162488dc400000001/train/python
+# https://www.codewars.com/kata/53697be005f803751e0015aa/train/python
