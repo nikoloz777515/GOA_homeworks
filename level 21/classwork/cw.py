05a1d93a97daf7505baa4023304912d52b310c04
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-05-27 22:51:40 +0400

    commit

--- level 21/classwork/cw.py
+++ level 21/classwork/cw.py
@@ -1,11 +1,24 @@
 # 1) https://www.codewars.com/kata/5467e4d82edf8bbf40000155/train/python
 
+# def descending_order(num):
+#     num=list(str(num))
+#     num.sort(reverse=True)
+#     result=""
+#     for i in num:
+#         result+=i
+#     return int(result)
+
 # მინიშნება: How to sort numbers list in desceding order in py?
 
 # how to transform number in string? or in reverse in py 
 
 # 2) https://www.codewars.com/kata/55908aad6620c066bc00002a/train/python
-
+# def xo(s):
+#     s = s.lower()
+#     if s.count("o") == s.count("x"):
+#         return True
+#     else:
+#         return False
 
 
 
--- level 21/homework/hw1.py
+++ level 21/homework/hw1.py
@@ -1,12 +1,58 @@
 # გააკეთეთ Codewars-ები:
 
 # 8 kyu:
-# 1) https://www.codewars.com/kata/57faece99610ced690000165
-# 2) https://www.codewars.com/kata/57fb09ef2b5314a8a90001ed
-# 3) https://www.codewars.com/kata/59dd3ccdded72fc78b000b25
-# 4) https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89
-# 5) https://www.codewars.com/kata/58bf9bd943fadb2a980000a7
+# 1) https://www.codewars.com/kata/57faece99610ced690000165def 
+# remove(st):
+    # return st.rstrip("!")
 
+# 2) https://www.codewars.com/kata/57fb09ef2b5314a8a90001ed
+# def replace_exclamation(st):
+#     result=""
+#     vowel="aeiouAEIOU"
+#     for i in st:
+#         if i in vowel:
+#             result+="!"
+#         else:
+#             result+=i
+#     return result
+
+
+# 3) https://www.codewars.com/kata/59dd3ccdded72fc78b000b25
+# def whatday(num):
+#     if num==1:
+#         return "Sunday"
+#     elif num==2:
+#         return "Monday"
+#     elif num==3:
+#         return "Tuesday"
+#     elif num==4:
+#         return "Wednesday"
+#     elif num==5:
+#         return "Thursday"
+#     elif num==6:
+#         return "Friday"
+#     elif num==7:
+#         return "Saturday"
+#     else:
+#         return "Wrong, please enter a number between 1 and 7"
+                    
+# 4) https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89
+# def mouth_size(animal): 
+#     if animal.lower() == "alligator":
+#         return "small"
+#     else:
+#         return "wide"
+        
+# 5) https://www.codewars.com/kata/58bf9bd943fadb2a980000a7
+# def who_is_paying(name):
+#     result=[]
+#     if len(name)<=2:
+#         return [name]
+#     else:
+#         result.append(name)
+#         result.append (name[:2])
+#     return result
+        
 
 # 7kyu:
 # 1) https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/python
