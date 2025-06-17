dd8489a0604f00fdadfee4c94e75445139cd4de3
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-06-17 22:54:41 +0400

    commit

---
+++ level 50/classwork/cw.py
@@ -0,0 +1,31 @@
+# 1)
+
+# 2)def solve(arr):
+#     for i in arr:
+#         if -i not in arr:
+#             return i
+
+# 3)
+
+
+# 4)
+
+# 5)def sort_array(source_array):
+    # odd_numbers = []
+    # result = []
+    # index = 0
+    
+    # for i in source_array:
+    #     if i % 2 != 0:
+    #         odd_numbers.append(i)
+    
+    # odd_numbers.sort();
+    
+    # for i in source_array:
+    #     if i % 2 != 0:
+    #         result.append(odd_numbers[index])
+    #         index += 1
+    #     else:
+    #         result.append(i)
+    
+    # return result
