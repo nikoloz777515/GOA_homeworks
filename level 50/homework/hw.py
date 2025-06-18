2c96aa9c49f06c4dd0e392edc75cfc5eda2ed76b
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-06-18 15:38:30 +0400

    commit

---
+++ level 50/homework/hw.py
@@ -0,0 +1,49 @@
+# 1) კომენტარების სახით ახსენით მოცემული კოდი
+# def sort_array(source_array):
+    # შევქმენით ცარიელი სია
+#     odd_numbers = []
+
+# შევქმენით ცარიელი სია
+#     result = []
+
+    # შეცქმენით ცარიელი ცვლადი
+#     index = 0
+
+    # გადავუარეთ for ციკლით source_array ს
+#     for num in source_array:
+
+        # გამოვიყენეთ if და გავუწერეთ თუ num % გაყოფისას ორზე არუდრის 2  მაგ შემთხვევაში შესრულდება ქვემოთ მოცემული append()
+#         if num % 2 != 0:
+
+            # აქ odd_numbers append() ით ჩავუმატეთ num
+#             odd_numbers.append(num)
+
+    # აქ sort() ით დავალაგეთ სია
+#     odd_numbers.sort();
+
+    # აქ for ით გადავუარეთ სიას
+#     for num in source_array:
+
+        # გამოვიყენეთ if
+#         if num % 2 != 0:
+
+            # აქ result append ით ჩავუმატეთ ჩვენი სია
+#             result.append(odd_numbers[index])
+#             index += 1
+#         else:
+            # აქ კი სხვა დანარჩენ შემთხვევაში ჩაემატება num
+#             result.append(num)
+    
+    # აქ კი დავაბრუნებინეთ result
+#     return result
+
+# 2) შეასრულეთ Codewars - ები
+
+# 7kyu
+# https://www.codewars.com/kata/5a3dd29055519e23ec000074/train/python
+# https://www.codewars.com/kata/56d02e6cc6c8b49c510005bb/train/python
+# https://www.codewars.com/kata/5390bac347d09b7da40006f6/train/python
+
+# 6 kyu
+# https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/train/python
+# https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/python
