bf6b63f043084c3d8b4000ff7b3cd199c837a7fb
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-06-22 19:21:52 +0400

    commit

--- level 50/classwork/cw.py
+++ level 50/classwork/cw.py
@@ -1,14 +1,41 @@
 # 1)
+# def sum_diagonals(matrix):
+#     size = len(matrix);
+#     sum = 0
+#     n = size - 1;
+    
+
+#     for i in range(size):
+#         sum += matrix[i][i]
+#         sum += matrix[i][n]
+#         n -= 1
+        
+#     return sum
+
+
 
 # 2)def solve(arr):
 #     for i in arr:
 #         if -i not in arr:
 #             return i
 
-# 3)
+# 3)def seqlist(first, c, l):
+#     result = []
+#     for i in range(l):
+#         result.append(first + i * c)
+#     return result
 
 
-# 4)
+# 4)def alphabet_position(text):
+#     alp = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+#     result = ""
+#     text = text.lower()
+#     for i in text:
+#         if i in alp:
+#             num = alp.index(i) + 1
+#             result += str(num) + " " 
+#     return result[:-1]
+
 
 # 5)def sort_array(source_array):
     # odd_numbers = []
