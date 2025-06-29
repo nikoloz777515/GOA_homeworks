e928c985c55632ddd859fdf0a952e3df950fd278
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-06-29 15:40:05 +0400

    commit

---
+++ level 55/homework/script2.js
@@ -0,0 +1,28 @@
+let a = 5 + 5.6
+let number = a = Math.floor(a)
+console.log(a)
+
+
+let b = 6 - 4
+console.log(b)
+
+let c = 33 * 4.9
+let num = c = Math.ceil(c)
+console.log(c)
+
+
+let d = 12 / 3.7
+let nm = d = Math.floor(d)
+console.log(d)
+
+
+let e = 4 % 2
+console.log(e)
+
+
+
+
+
+
+
+
