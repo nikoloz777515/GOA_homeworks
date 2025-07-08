36f640b142ac7882a9d1ae99b174539f56c98b04
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-07-08 22:50:16 +0400

    commit

--- level 58/homework/script8.js
+++ level 58/homework/script8.js
@@ -1,1 +1,18 @@
 // 8) შექმენით ერთი ფუნქცია გადაეცით მას ერთი პარამეტრი num, თქვენმა ფუნქციამ უნდა შეამოწმოს არის თუ არა კონკრეტული რიცხვი ლუწი თუ არის დააბრუნეთ რომ რიცხვი არის "Even" ლუწი სხვა შემთხვევაში კი დააბრუნეთ რომ "The Number Is Odd" ეს შემოწმება გააკეთეთ ternary operator - ის გამოყენებით, გამოიძახეთ ფუნქცია 4 ჯერ და გადაეცით მას პარამეტრად სხვადასხვა მნიშვნელობები
+
+
+
+function check(num) {
+  return(num % 2 === 0) ? "The Number Is Even" : "The Number Is Odd"
+}
+let numb1= check(9);
+console.log(numb1);
+
+let numb2= check(2);
+console.log(numb2);
+
+let numb3= check(99);
+console.log(numb3);
+
+let numb4= check(12);
+console.log(numb4);
