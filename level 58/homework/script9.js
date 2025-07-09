433f416e92ab5fb9b6665e9ea04ae062b5bcaddb
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-07-09 16:56:15 +0400

    commit

--- level 58/homework/script9.js
+++ level 58/homework/script9.js
@@ -1,1 +1,16 @@
 //  9) შექმენით ორი ღილაკი heads tails ორივეს მიანიჭეთ უნკიალური ID შექმენით ერთი პარაგრაფი და მასაც მიანიჭეთ უნკიალური ID, მოიპოვეთ წვდომა ორივე ღილაკზე, იმ შემთხვევაში თუ მომხმარებელმა დააჭირა ხელი heads ღილაკს მაშინ paragraph element - ის ტექსტის შიგთავსი შეცვალეთ და გახადეთ heads - ი, თუ მომხმარებელმა დააჭირა ხელი tails ღილაკს მაშინ paragraph element - ის ტექტსი შიგთავსი შეცვალეთ და გახადეთ tails - ის ტოლი, მინიშნება გამოიყენეთ document.getElementById, addEventListener, მოუსმინეთ 'click' მოვლენას 
+
+let paragraph = document.getElementById("pgh")
+
+
+let button = document.getElementById("btn1")
+    button.addEventListener("click",() =>{
+        paragraph.textContent = "heads"
+    })
+
+
+let button2 = document.getElementById("btn2")
+     button2.addEventListener("click",() =>{
+        paragraph.textContent = "tails"
+    })
+
---
+++ level 59/homework/script2.js
@@ -0,0 +1,21 @@
+let btn = document.getElementById("btn1")
+   btn.addEventListener("click",()=>{
+    paragraph.style.backgroundColor = "green"
+   }
+) 
+
+
+let button = document.getElementById("btn2")
+
+let paragraph =document.getElementById("pgh")
+
+button.addEventListener("click", () =>{
+   paragraph.style.color = "red" 
+})
+
+let btnn = document.getElementById(btn3)
+        btn3.addEventListener("click",()=>{
+            paragraph.style.fontSize ="70px"
+        })
+
+
