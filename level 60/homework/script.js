4037e3bb292170971576414c63e0d66ca16df339
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-07-11 02:02:21 +0400

    commit

---
+++ level 60/homework/script.js
@@ -0,0 +1,5 @@
+// 1) შექმენით ერთი მასივი შეინახეთ მასში 5 სახეობის ხილი push Method - ის გამოყენებით სიის ბოლოს დაამატეთ რამოდენიმე ელემენტი 
+
+let fruits = ['Banana','Carrot','Pineaple','Strawberry']
+fruits.push('Cherry','Grapes')
+        console.log(fruits)
