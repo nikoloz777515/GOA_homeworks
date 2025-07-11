fd1ff9d5100b2c8f91fbef6c08795851e52f318c
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-07-11 18:31:26 +0400

    commit

--- level 60/homework/script3.js
+++ level 60/homework/script3.js
@@ -1,1 +1,113 @@
 //  3) მოიძიეთ დამატებითი მეთოდები მასივებზე და გააკეთეთ თითოეულზე 3 მაგალითი (მოიძიეთ ინფორმაცია მასივის შემდეგ მეთოდებზე slice, splice, shift, unshift, IndexOf) 
+
+
+// let cars = ['Mercedes-Benz','BMW','FORD','Reno']
+// let slices = cars.slice(1)
+// console.log(slices)
+
+
+
+// let fruits = ['pineaole','banana','grape','carrot']
+// let slice = fruits.slice(-1)
+// console.log(slice)
+
+
+
+//  let alpabet = ['a','b','c','d','e']
+//  let asph = alpabet.slice(0,2)
+//  console.log(asph)
+
+
+
+
+
+
+
+
+
+// let cars = ['Mercedes-Benz','BMW','FORD','Reno']
+// cars.splice(1 ,1, 'cls','clk')
+//  console.log(cars)
+
+
+
+// let fruits = ['pineaple','banana','grape','carrot']
+// let splice= fruits.splice(1,2)
+// console.log(splice)
+
+
+
+//  let alpabet = ['a','b','c','d','e']
+//  let asph = alpabet.splice(-1)
+//  console.log(asph)
+
+
+
+
+
+
+
+
+// let cars = ['Mercedes-Benz','BMW','FORD','Reno']
+// let slices = cars.shift(1)
+// console.log(slices)
+
+
+// let fruits = ['pineaple','banana','grape','carrot']
+// let splice= fruits.shift()
+// console.log(splice)
+
+
+//  let alpabet = ['a','b','c','d','e']
+//  let asph = alpabet.shift()
+//  console.log(asph)
+
+
+
+
+
+
+
+// let cars = ['Mercedes-Benz','BMW','FORD','Reno']
+//  cars.unshift('Buggatti')
+// console.log(cars)
+
+
+
+// let fruits = ['pineaple','banana','grape','carrot']
+// fruits.unshift('Kiwi','Apple')
+// console.log(fruits)
+
+
+
+//  let alphabet = ['a','b','c','d','e']
+//   alphabet.unshift('A','B','C','D','E')
+//  console.log(alphabet)
+
+
+
+
+
+
+
+// let cars = ['Mercedes-Benz','BMW','FORD','Reno']
+//  console.log(cars.indexOf('Reno'))
+
+
+//  let fruits = ['pineaple','banana','grape','carrot']
+// console.log(fruits.indexOf('grape'))
+
+
+//  let alphabet = ['a','b','c','d','e']
+//  console.log(alphabet.indexOf('d'))
+
+
+
+
+
+
+
+
+
+
+
--- level 60/homework/script6.js
+++ level 60/homework/script6.js
@@ -3,3 +3,36 @@
 // ორი ღილაკი -- Change Text Content, Add innerHTML
 // მოიპოვეთ მათზე წვდომა, როდესაც მომხმარებელი დააკლიკებს პირველ ღილაკს ხელს უნდა მოხდეს text - ის შიგთავსის შეცვლა, ხოლო როდესაც მომხმარებელი დააკლიკებს მეორე ღილაკს ხელს innerHTML - ის გამოყენებით უნდა ჩაამატოთ მეორე ელემენტში დამატებით ელემენტი <p><b>Value</b></p>, <p><strong></strong></p>, <p><i>Italic</i></</p> და ასე შემდეგ, კომენტარების სახით ახსენით თუ რა განსხვავებაა:
 // div.innerHTML = `<p></p>`, div.innerHTML += `<p></p>` შორის 
+
+
+let firstP = document.getElementById("p1")
+
+let secP = document.getElementById("p2")
+
+let firstBT = document.getElementById("btn1")
+firstBT.addEventListener('click',() =>{
+    firstP.textContent="BLA BLA"
+})
+
+
+
+let sectBT = document.getElementById("btn2")
+
+sectBT.addEventListener('click',() => {
+    secP.innerHTML =
+    '<p><b>Value</b></p> <p><strong>Strong</strong></p> <p><i>Italic</i></p>';
+});
+
+
+// კომენტარების სახით ახსენით თუ რა განსხვავებაა:
+//  div.innerHTML = `<p></p>`, div.innerHTML += `<p></p>` შორის 
+
+// განსხვავება ისაა რომ  div.innerHTML = `<p></p>`, div ასე შეიცვლება ტექსტი მაგრამ ბევრჯერაც რომ დავაჭიროთ აზრი არექნება მარტო ერთხელ შეიცვლება
+
+// innerHTML += `<p></p> და ამის გამოყენებისას მაგალითდ რამდენჯერაც ღილაკზე დავაჭერთ ხელს დაემატება ზემოდან და გახდება ხილვადი ჩვენთვის ეგ პარაგრაფი
+
+
+
+
+
+    
