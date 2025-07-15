8a6a95d102c9610f220b63cd5956878aa014e06b
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-07-16 01:59:27 +0400

    commit

---
+++ level 62/homework/script.js
@@ -0,0 +1,10 @@
+//  1) შექმენით მასივი შეინახეთ მასში რაიმე მნიშვნელობები, while loop - ის დახმარებით გადაუარეთ ამ მასივს და დაბეჭდეთ თითოული ელემენტი
+
+let array = ['Bmw','Mercedes','Audi','Wolksvagen']
+
+let i = 0
+
+while (i < array.length){
+    console.log(array[i])
+    i++
+}
