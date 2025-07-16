4c3df9119f22e53dc32b017d30bd29c3b06eb779
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-07-16 22:17:27 +0400

    commit

--- level 61/homework/script3.js
+++ level 61/homework/script3.js
@@ -1,6 +1,14 @@
 //  3) შექმენით ერთი მასივი შეინახეთ მასში ხილის სახეობები მაგალითად [apple, cherry, strawberry, apple, orange], თქვენი დავალებაა გადაუაროთ მოცემულ სიას და დაითვალოთ თუ რამდენჯერ გხვდებათ (მაგალითად: 'Apple') ხოლი სიაში 
 
-const array = ['apple', 'mango', 'strawberry', 'apple', 'orange', 'cherry'];
+const fruitsArray = ['apple', 'cherry', 'strawberry', 'apple', 'orange','apple']
 
+let countFruit = 0;
 
+for(let i = 0; i < fruitsArray.length; i++){
+    if(fruitsArray[i] === 'apple'){
+        countFruit += 1
+    }
+}
+
+console.log(countFruit)
 
