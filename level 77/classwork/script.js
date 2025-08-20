4b056e6b95200e861f4140872d6ff8ade47846c8
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-08-20 16:16:33 +0400

    commit

--- level 77/classwork/script.js
+++ level 77/classwork/script.js
@@ -1,16 +1,52 @@
 
 
 
-function cheeck (em){
-    let form = document.querySelector(form);
+function cheeck (){
+    let form = document.querySelector('form');
+    let chck = document.querySelector('button');
+        let email = form.querySelector('#email');
+    let label = document.querySelector('label')
+    let main = document.getElementById('main')
 
-    let chck = document.querySelector(button)
+    chck.addEventListener("click",(e) => {
+            e.preventDefault()
 
-    
-    
-    if(!em =='@' || !em =='1,2,3,4,5,6,7,8,9,10'){
-    
-        alert("enter valid email")
-        return;
-    }
+
+            
+      if(email.value.includes("@") && email.value.includes(".com")){
+            main.innerHTML = `
+            <div class = 'suc'> 
+
+            <img  class ="sucs" src ="./assets/images/icon-success.svg" />
+
+            <h1>Thanks for <br> subscribing!</h1>
+            
+            <p>  A confirmation email has been sent <br> to ${email} 
+  Please open it and click <br> the button inside to confirm your subscription </p>
+
+  <button>Dismiss message</button
+
+
+
+            
+            </div>
+            
+            
+            
+            
+            `
+            
+        }
+        else{
+
+            email.style.backgroundColor = 'hsla(4, 100%, 67%, 0.35)'
+            email.style.borderColor = 'hsl(4, 100%, 67%)'
+            label.innerText = " Please enter a valid email"
+            
+        }
+
+    });
+
 }
+
+cheeck()
