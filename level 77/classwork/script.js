c69ffaf80dc20fed87d8b59433a53aede55e53c2
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-08-20 15:11:07 +0400

    commit

---
+++ level 77/classwork/script.js
@@ -0,0 +1,16 @@
+
+
+
+function cheeck (em){
+    let form = document.querySelector(form);
+
+    let chck = document.querySelector(button)
+
+    
+    
+    if(!em =='@' || !em =='1,2,3,4,5,6,7,8,9,10'){
+    
+        alert("enter valid email")
+        return;
+    }
+}
