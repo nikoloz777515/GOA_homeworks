7d2233767f65ad5fdcb1e2137dc69c42bb33b34d
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-08-21 15:25:09 +0400

    commit

--- level 74/classwork/script.js
+++ level 74/classwork/script.js
@@ -1,5 +1,43 @@
 let rmv = document.querySelectorAll(".rMv")
 
+let button = document.getElementById("bt2")
+
+let body = document.body
+
+let night = document.getElementById("btn7")
+
+let ext = document.querySelectorAll(".ext-card")
+
+let idk = document.querySelectorAll(".idk")
+
+let ext2 = document.getElementById("id1")
+
+
+
+button.addEventListener("click",()=>{
+    body.style.backgroundColor = "rgb(238, 246, 253)"
+    for(let i of ext){
+        i.style.backgroundColor = "white"
+    }
+    for(let i of idk){
+        i.style.color = "black"
+    }
+    ext2.style.backgroundColor = "white"
+})
+
+
+night.addEventListener("click",() =>{
+     body.style.backgroundColor = " hsl(227, 75%, 14%)"
+    for(let i of ext){
+        i.style.backgroundColor = "hsl(226, 25%, 17%)"
+
+    }
+    for(let i of idk){
+        i.style.color = "white"
+    }
+    ext2.style.backgroundColor = "hsl(226, 25%, 17%)"
+})
+
 rmv.forEach(bt =>{
     bt.addEventListener("click", () =>{
         bt.closest(".ext-card").remove()
