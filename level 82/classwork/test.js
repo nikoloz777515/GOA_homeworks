c8e15615c33a2b0e16ebffcb6c7268a2ed64e974
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-09-03 20:56:43 +0400

    commit

--- level 82/classwork/test.js
+++ level 82/classwork/test.js
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+const numbers = [5, 12, 4,6,7,8]
+const oddNumbers = []
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 !== 0) {
+    oddNumbers.push(numbers[i]); 
+  }
+}
+
