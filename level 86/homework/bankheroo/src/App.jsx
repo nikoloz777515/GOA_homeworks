72cbb721615a962b69bcffac8a566b196f976a01
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-09-12 20:48:07 +0400

    commit

--- level 86/homework/bankheroo/src/App.jsx
+++ level 86/homework/bankheroo/src/App.jsx
@@ -1,11 +1,46 @@
-
+// 3) შექმენით vite გამოყენებით, პროექტი სახელად bankhero, რომლის app ფაილშიც დაამატეთბ main elements 5 სექციით
 
 function App() {
 return(
-  <div>
-    <h1>Hello my dear React</h1>
-  </div>
+<main>
+  <section>
+    <h1>HELLO EVERYONE THIS IS MY SITE</h1>
+  </section>
+
+  <section>
+    <h1>HELLO THIS IS REACT.</h1>
+  </section>
+
+  <section>
+    <h1>HELLO THIS IS HTML</h1>
+  </section>
+
+  <section>
+    <h1>
+    HELLO THIS IS CSS.</h1>
+  </section>
+
+  <section>
+    <h1>
+   HELLO THIS IS C++</h1>
+  </section>
+</main>
 )
 }
 
+
 export default App
+
+// // src/App.jsx
+// import Nav from './nav';  // აქ შემოგაქვს Nav კომპონენტი
+
+// function App() {
+//   return (
+//     <div>
+//       <Nav /> {/* აქ ვიძახებთ Nav კომპონენტს */}
+//     </div>
+//   );
+// }
+
+// export default App;
+
---
+++ level 86/homework/bankheroo/src/nav.jsx
@@ -0,0 +1,16 @@
+// src/Nav.jsx
+function nav (){
+    return(
+        <nav>
+        <ul>
+            <li>app</li>
+             <li>home</li>
+              <li>about us</li>
+               <li>sign up</li>
+                <li>login</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default nav;
