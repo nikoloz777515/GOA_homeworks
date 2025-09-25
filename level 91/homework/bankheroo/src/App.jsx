e198c79242d1c4a991afc928ac61561649809549
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-09-25 19:52:16 +0400

    commit

---
+++ level 91/homework/bankheroo/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import Head from './Head'
+
+
+
+
+function App() {
+
+
+  return (
+    <main>
+      <Head  name = "niko"/>
+
+      
+    </main>
+  )
+}
+
+export default App
+
+
+
+// 2) შექმენით მთავარი კომპონენტი, გასტილეთ ის external css - ის გამოყენებით, მოიძეთ ინფორმაცია თუ როგორ ხდება React - ში external css - ის გამოყენება, შეგიძლიათ დაყოთ პატარ-პატარა კომპონენტებად და საბოლოოდ გამოიძახოთ ისინი App კომპონენტში
+
+// 3) გააკეთეთ მაგალითები codecademy - ში ნასწავლ მასალასთან დაკავშირებით
---
+++ level 91/homework/bankheroo/src/Head.jsx
@@ -0,0 +1,26 @@
+import "./index.css";
+
+
+function Head({name}){
+   return(
+<div>
+     <h1 className="firsth1">
+        hello to all
+    </h1>
+
+    <h2 className="h2">
+        the name is {name}
+    </h2>
+</div>
+
+
+   )
+}
+
+
+export default Head
+
+
+// 2) შექმენით მთავარი კომპონენტი, გასტილეთ ის external css - ის გამოყენებით, მოიძეთ ინფორმაცია თუ როგორ ხდება React - ში external css - ის გამოყენება, შეგიძლიათ დაყოთ პატარ-პატარა კომპონენტებად და საბოლოოდ გამოიძახოთ ისინი App კომპონენტში
+
+// 3) გააკეთეთ მაგალითები codecademy - ში ნასწავლ მასალასთან დაკავშირებით
