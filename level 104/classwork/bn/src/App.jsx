bc33cb74bef37a975c3b924b7104927275b18f17
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-10-25 20:25:12 +0400

    commit

---
+++ level 104/classwork/bn/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
+
+
+// 1) გამოიყენეთ show, checkout და reset ბრძანებები, აგრეთვე ახსენით რა არის HEAD + გამოიყენეთ წინა გაკვეთილზე ნასწავლი ბრძანეწბები და ფაილში ჩამოწერეთ თითოეული ბრძანების ახსნა შემდეგ კი ცვლილებები შეინახედ ვერსიად სახელად "Explained common git commands"
