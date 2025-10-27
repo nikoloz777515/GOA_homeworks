c9066c030752b85fdfd82e4cd2149360f4bccbf7
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-10-28 00:44:28 +0400

    commit

--- level 104/classwork/bn/src/GitFind.jsx
+++ level 104/classwork/bn/src/GitFind.jsx
@@ -18,18 +18,19 @@ function GitFind(){
     }
     return(
         <div className="bg-[rgb(20,28,47)] min-h-screen">
-      <h1 className="text-white text-5xl">devFinder</h1>
+      <h1 className="text-white text-5xl text-center ">devFinder</h1>
 
-      <input className="w-[300px] bg-[rgb(18,29,50)]"
+
+      <input className="w-[500px] h-[60px] bg-[rgb(18,29,50)] ml-[400px] relative top-[100px] "
         type="text"
         placeholder="Enter GitHub username"
         value={userName}
         onChange={(e) => setUsername(e.target.value)}
       />
-      <button onClick={fetchUsr} className="bg-[rgb(16,109,217)] text-white">Search</button>
+      <button onClick={fetchUsr} className="bg-[rgb(16,109,217)] text-white relative top-[50px] ml-[740px] rounded-lg h-[40px] w-[80px]">Search</button>
 
       {user && (
-        <div className="bg-[rgb(31,42,71)] ">
+        <div className="bg-[rgb(31,42,71)] text-white relative top-[250px] w-[600px] ml-[450px] h-[300px]">
           <h2>{user.name}</h2>
           <p>{user.login}</p>
           <img
@@ -38,7 +39,7 @@ function GitFind(){
             width="100"
             style={{ borderRadius: "50%" }}
           />
-          <p>Followers: {user.followers}</p>
+          <p className="text-red-600">Followers: {user.followers}</p>
           <p>Following: {user.following}</p>
           <p>Public repos: {user.public_repos}</p>
           <a href={user.html_url} target="_blank">
