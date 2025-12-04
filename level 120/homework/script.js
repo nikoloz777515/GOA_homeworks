f7a5270d81c1a95feac995843968717fc9fb9891
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2025-12-04 22:09:38 +0400

    commit

---
+++ level 120/homework/script.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/59fa8e2646d8433ee200003f/train/python
+
+
+// https://www.codewars.com/kata/52449b062fb80683ec000024/train/javascript
+
+
+
+// https://www.codewars.com/kata/5a00a8b5ffe75f8888000080/train/python
