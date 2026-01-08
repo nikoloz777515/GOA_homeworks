ed9af27489599374a8e08eb81f1d733bd26ccfa1
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2026-01-08 20:59:13 +0400

    commit

---
+++ work/idk.py
@@ -0,0 +1,32 @@
+# def greet():
+#   print("Hello World")
+
+# greet()
+
+# def userGreet(name):
+#   print('Hello',name)
+
+# userGreet('Nika')
+# userGreet('Gio')
+# userGreet('dato')
+
+# def add(a,b):
+#   print(a + b)
+
+# add(5,7)
+
+# def substract(a,b):
+#   print(a - b)
+
+# substract(4,2)
+
+
+
+def age():
+  userAge = int(input("შემოიტანე შენი ასაკი: "))
+  if userAge >= 18:
+    print("შენ ხარ სრულწლოვანი ")
+  else:
+    print("შენ არ ხარ არასრულწლოვანი")
+
+age()
