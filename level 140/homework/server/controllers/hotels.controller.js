6bfe8d3043e0019505710a200ece885aff07b06e
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2026-01-29 01:25:21 +0400

    commit

---
+++ level 140/homework/server/controllers/hotels.controller.js
@@ -0,0 +1,19 @@
+const {readFile,writeFile} = require('../utils/heper')
+
+const createBooking =(req,res) =>{
+  const bookings = readFile('database/booking.json')
+    const booking ={
+      id: Date.now(),
+      ...req.body
+    }
+    bookings.push(booking)
+    writeFile('database/booking.json',bookings)
+    res.status(200).json({
+      message: 'booking created',booking
+    })
+}
+
+const getBooking =(req,res) =>{
+  const booking = readFile('database/booking.json')
+  res.status(200).json(booking)
+}
