fbf5519fd7807dd53d1aef7d28d1a1769284fdfa
nikoloz777515 <nikolozkvelashvili33@gmail.com>
2026-02-21 19:04:02 +0400

    commit

--- level 150/homework/routers/Auth.roter.js
+++ level 150/homework/routers/Auth.roter.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router2 = express.Router()
 
-const {signUp,login} = require('../controllers/auth.controller') 
+const {signUp,login} = require('../controllers/auth.controller.js')
 
 router2.post('/signUp',signUp)
 router2.post('/login',login)
